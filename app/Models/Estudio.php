5b40c7dba2e37e622d4fce1caf9b0f0f9ec591c2
Sebastian <sebastianmaydana@gmail.com>
2025-08-29 19:45:40 -0300

    modelos y migraciones

---
+++ app/Models/Estudio.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Estudio extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'estudios';
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function paciente()
+    {
+        return $this->belongsTo(Paciente::class);
+    }
+
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->domain('sistema.petrillilab.local')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
